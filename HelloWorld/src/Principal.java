866148cab42a91e2ff3aa0a423b1c0edf3418929
Loir <arboniguitar@gmail.com>
2014-03-21 23:54:53 -0300

    HelloWolrd adicionado no repositorio

---
+++ HelloWorld/src/Principal.java
@@ -0,0 +1,13 @@
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
